fff545d8536027ea81c24fa31895f4ea70133ef0
Caleb Rogers <rogersjcaleb@gmail.com>
2018-11-11 19:36:57 -0800

    WIP: Attempting to make a request to aws lambda

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+
+const lambda_key = process.env.REACT_APP_AWS_LAMBDA_API_KEY;
 const marker_array = [
     {
         title: 'The marker`s title will appear as a tooltip.',
@@ -24,6 +26,19 @@ const markers = marker_array.map((marker)=>{
 });
 
 export class MapContainer extends Component {
+    componentDidMount(){
+        let myHeaders = new Headers();
+        myHeaders.append('x-api-key', lambda_key);
+        fetch('https://dauyyicmya.execute-api.us-east-1.amazonaws.com/default/getLostLimeBikes', {
+            headers: myHeaders,
+        })
+        .then((res)=>{
+            console.log('success,', res)
+        })
+        .catch((err)=>{
+            console.log('err', err);
+        });
+    }
     render() {
         return (
             <Map google={this.props.google} zoom={14}>
