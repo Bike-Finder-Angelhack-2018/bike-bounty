186c1de6fea2d731169821c746c6ca1e78192e2e
Caleb Rogers <rogersjcaleb@gmail.com>
2018-11-11 18:34:46 -0800

    Display an array of Markers

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+const marker_array = [
+    {
+        title: 'The marker`s title will appear as a tooltip.',
+        name: 'SOMA' ,
+        position:{ lat: 37.778529, lng: -122.405640 }
+    },
+    {
+        title: 'The marker`s title will appear as a tooltip.',
+        name: 'SOMA',
+        position: { lat: 37.778419, lng: -122.405640 }
+    },
+    {
+        title: 'The marker`s title will appear as a tooltip.',
+        name: 'SOMA',
+        position: { lat: 37.778639, lng: -122.405640 }
+    },
+];
+const markers = marker_array.map((marker)=>{
+    return <Marker title={marker.title} name={marker.name} position={marker.position}></Marker>
+});
 
 export class MapContainer extends Component {
     render() {
         return (
             <Map google={this.props.google} zoom={14}>
-
+            {markers}
             </Map>
         );
     }
