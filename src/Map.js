d3965085d39c7f910186757357f8fa2002eb557d
Caleb Rogers <rogersjcaleb@gmail.com>
2018-11-15 17:28:41 -0800

    Add basic infowindow handling

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper, InfoWindow } from 'google-maps-react';
 
 
 const lambda_key = process.env.REACT_APP_AWS_LAMBDA_API_KEY;
@@ -11,9 +11,12 @@ export class MapContainer extends Component {
         this.state = {
             isLoaded: false,
             bikes: [],
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
         };
     }
-    componentDidMount(){
+    componentDidMount = ()=>{
         let myHeaders = new Headers();
         let self = this;
         myHeaders.append('x-api-key', lambda_key);
@@ -31,16 +34,38 @@ export class MapContainer extends Component {
             console.log('err', err);
         });
     }
+    onMarkerClick = (props, marker, e)=>{
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+    }
+    onMapClicked = (props)=>{
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    }
     render() {
         const { isLoaded, bikes } = this.state;
         if (!isLoaded){
             return <div>Loading...</div>
         } else {
             return (
-                <Map google={this.props.google} zoom={14}>
+                <Map google={this.props.google} zoom={14} onClick={this.onMapClicked}>
                 {bikes.map((bike)=>{
-                    return <Marker key={bike.id} title={bike.title} name={bike.name} position={bike.position} icon={{url: 'bike.png'}}></Marker>
+                    return <Marker onClick={this.onMarkerClick} key={bike.id} title={bike.title} name={bike.name} position={bike.position} icon={{url: 'bike.png'}}></Marker>
                 })}
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
                 </Map>
             );
         }
